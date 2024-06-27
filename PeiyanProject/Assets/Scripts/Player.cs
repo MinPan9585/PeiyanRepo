1eb07f686530a6cf59d83b4a1254c5c6a00efccf
Zoupy22 <2776384961@qq.com>
2024-06-28 00:41:38 +0800

    add velocity

--- PeiyanProject/Assets/Scripts/Player.cs
+++ PeiyanProject/Assets/Scripts/Player.cs
@@ -20,12 +20,30 @@ public class Player : MonoBehaviour
     private float jumpForce = 0f;
     private float jumpTime = 0f;
     private bool isJumping = false;
-    
+    public Vector3 direction;
 
     // Update is called once per frame
     void Update()
     {
 
+        //xiangliang
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePosition = Input.mousePosition;
+            Ray ray = Camera.main.ScreenPointToRay(mousePosition);
+
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                
+                Vector3 mouseToObjectA = transform.position  - hit.point;
+
+                // 在这里可以使用向量来进行相应的操作
+                Debug.Log("鼠标位置与物体A的向量：" + mouseToObjectA);
+                direction = new Vector3(mouseToObjectA.x, 0, mouseToObjectA.z);          
+             } //空格
+          
+        }
         if (Input.GetKeyDown(KeyCode.Space) && !isJumping) // 检测空格键按下
         {
             isJumping = true;
@@ -40,7 +58,7 @@ public class Player : MonoBehaviour
         if (Input.GetKeyUp(KeyCode.Space) && isJumping) // 检测空格键释放
         {
             jumpForce = Mathf.Lerp(jumpForceMin, jumpForceMax, jumpTime / maxJumpTime);
-            GetComponent<Rigidbody>().AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            GetComponent<Rigidbody>().AddForce(Vector3.up * jumpForce+direction , ForceMode.Impulse);
 
             isJumping = false;
         }
